2d9b0b8bec13571efd7ee48bbdcfd74f3933d381
AydenGrace <pa.ce.hepl@gmail.com>
2024-07-22 16:05:12 +0200

    Turbulence in water test

--- FRONT/src/components/BattleArea/BattleArea.jsx
+++ FRONT/src/components/BattleArea/BattleArea.jsx
@@ -168,6 +168,28 @@ export default function BattleArea({
   return (
     <section className={`${style.BattleArea}`}>
       <div className={`d-flex flex-wrap ${style.container}`}>
+        <svg style={{ width: "0" }}>
+          <filter id="turbulence" x="0" y="0" width="100%" height="100%">
+            <feTurbulence
+              id="sea-filter"
+              numOctaves="3"
+              seed="2"
+              baseFrequency="0.02 0.05"
+            ></feTurbulence>
+            <feDisplacementMap
+              scale="20"
+              in="SourceGraphic"
+            ></feDisplacementMap>
+            <animate
+              xlink:href="#sea-filter"
+              attributeName="baseFrequency"
+              dur="60s"
+              keyTimes="0;0.5;1"
+              values="0.02 0.06;0.04 0.08;0.02 0.06"
+              repeatCount="indefinite"
+            />
+          </filter>
+        </svg>
         {!isEnemyMap &&
           (ShipPositions
             ? ShipPositions.map((ship, idx) => (
@@ -204,25 +226,6 @@ export default function BattleArea({
           ))
         )}
       </div>
-      <svg>
-        <filter id="turbulence" x="0" y="0" width="100%" height="100%">
-          <feTurbulence
-            id="sea-filter"
-            numOctaves="3"
-            seed="2"
-            baseFrequency="0.02 0.05"
-          ></feTurbulence>
-          <feDisplacementMap scale="20" in="SourceGraphic"></feDisplacementMap>
-          <animate
-            xlink:href="#sea-filter"
-            attributeName="baseFrequency"
-            dur="60s"
-            keyTimes="0;0.5;1"
-            values="0.02 0.06;0.04 0.08;0.02 0.06"
-            repeatCount="indefinite"
-          />
-        </filter>
-      </svg>
 
       {thisMode === "selection" && (
         <>
