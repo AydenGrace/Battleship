0d4742c820f61b748739b26c57013a79c81ea096
AydenGrace <pa.ce.hepl@gmail.com>
2024-07-22 14:59:42 +0200

    Ready Btn

--- FRONT/src/router.jsx
+++ FRONT/src/router.jsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import Homepage from "./pages/Homepage/Homepage";
-import Prepare from "./pages/Prepare/Prepare";
 import Battle from "./pages/Battle/Battle";
 import RoomTest from "./pages/RoomTest/RoomTest";
 import Login from "./pages/Login/Login";
@@ -10,7 +9,6 @@ import Logout from "./pages/Logout/Logout";
 import Room from "./pages/Room/Room";
 import UserNotConnected from "./components/ProtectedRoutes/UserNotConnected";
 import UserConnected from "./components/ProtectedRoutes/UserConnected";
-import InRoom from "./components/ProtectedRoutes/InRoom";
 
 export const router = createBrowserRouter([
   {
