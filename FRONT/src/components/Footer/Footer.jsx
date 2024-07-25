f609c26ac3502a7b9e263f7aee36c8bee2580e72
AydenGrace <pa.ce.hepl@gmail.com>
2024-07-25 15:36:15 +0200

    Footer

--- FRONT/src/components/Footer/Footer.jsx
+++ FRONT/src/components/Footer/Footer.jsx
@@ -4,10 +4,12 @@ import style from "./Footer.module.scss";
 export default function Footer() {
   return (
     <div className={`f-center w-100 ${style.Footer}`}>
-      Copyright 2024 ©{" "}
-      <a href="https://www.linkedin.com/in/pierre-alexandre-crabbé-679058182/">
-        Crabbé Pierre-Alexandre
-      </a>
+      <p>
+        Copyright 2024 © <span> </span>
+        <a href="https://www.linkedin.com/in/pierre-alexandre-crabbé-679058182/">
+          Crabbé Pierre-Alexandre
+        </a>
+      </p>
     </div>
   );
 }
