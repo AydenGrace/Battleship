4eec2d4d94890b6cdea17f85f2f3f642c21a35fb
AydenGrace <pa.ce.hepl@gmail.com>
2024-07-26 12:04:55 +0200

    Get User History

--- FRONT/src/pages/Account/component/Historic/components/History_Item.jsx
+++ FRONT/src/pages/Account/component/Historic/components/History_Item.jsx
@@ -1,5 +1,53 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import style from "./History_Item.module.Scss";
+import { UserContext } from "../../../../../context/UserContext";
 
 export default function History_Item({ match }) {
-  return <div className={`w-100 f-center`}>History_Item</div>;
+  const [winner, setWinner] = useState(false);
+  const { user } = useContext(UserContext);
+
+  useEffect(() => {
+    switch (match.status) {
+      case "finish":
+        FinishSetup();
+        break;
+      case "give_up":
+        break;
+      case "battle":
+        break;
+      case "prepare":
+        break;
+      default:
+        break;
+    }
+  }, [match, user]);
+
+  const FinishSetup = () => {
+    let idx;
+    if (match.current_turn) idx = 0;
+    else idx = 1;
+    if (match.maps[idx].user === user._id) setWinner(true);
+    else setWinner(false);
+  };
+
+  return (
+    <div
+      className={`w-100 align-items-center justify-content-sb mb-10 ${style.container}`}
+    >
+      {match.status === "finish" ? (
+        winner ? (
+          <h3 className={`${style.status} ${style.green}`}>Victoire</h3>
+        ) : (
+          <h3 className={`${style.status} ${style.red}`}>Défaite</h3>
+        )
+      ) : match.status === "battle" ? (
+        <h3 className={`${style.status} ${style.orange}`}>Bataille en cours</h3>
+      ) : match.status === "give_up" ? (
+        <h3 className={`${style.status} ${style.red}`}>Abandon</h3>
+      ) : (
+        <h3 className={`${style.status} ${style.orange}`}>Préparation</h3>
+      )}
+      History_Item
+    </div>
+  );
 }
