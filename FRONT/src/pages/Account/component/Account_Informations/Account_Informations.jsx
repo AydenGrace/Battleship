d3045fb51dd23242cb41eae26b79c6c1061e2bc7
AydenGrace <pa.ce.hepl@gmail.com>
2024-07-26 13:21:27 +0200

    Get User History

--- FRONT/src/pages/Account/component/Account_Informations/Account_Informations.jsx
+++ FRONT/src/pages/Account/component/Account_Informations/Account_Informations.jsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import style from "./Account_Informations.module.scss";
 
+import { UserContext } from "../../../../context/UserContext";
+
 export default function Account_Informations() {
+  const { user } = useContext(UserContext);
   return (
     <div
-      className={`d-flex w-100 flex-fill flex-column align-items-center p-8`}
+      className={`f-center w-100 flex-fill flex-column align-items-center p-8`}
     >
-      <h2 className="mb-20">Informations Personnelles</h2>
+      <h2 className="card">Informations Personnelles</h2>
     </div>
   );
 }
