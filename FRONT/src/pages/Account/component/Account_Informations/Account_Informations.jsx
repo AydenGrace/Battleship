a40364a5c00bc9ef0b2e386b4aaa8cbaf6524972
AydenGrace <pa.ce.hepl@gmail.com>
2024-07-26 13:33:07 +0200

    Get User History

--- FRONT/src/pages/Account/component/Account_Informations/Account_Informations.jsx
+++ FRONT/src/pages/Account/component/Account_Informations/Account_Informations.jsx
@@ -9,7 +9,13 @@ export default function Account_Informations() {
     <div
       className={`f-center w-100 flex-fill flex-column align-items-center p-8`}
     >
-      <h2 className="card">Informations Personnelles</h2>
+      <div className="card d-flex flex-column">
+        <h2 className="mb-20">Informations Personnelles</h2>
+        <div className="w-100 d-flex g-10">
+          <p>Pseudonyme : </p>
+          <p>{user.username}</p>
+        </div>
+      </div>
     </div>
   );
 }
