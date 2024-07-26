5aea26e142fc4ccb36df2b489967ea1963cb31d7
AydenGrace <pa.ce.hepl@gmail.com>
2024-07-26 10:29:32 +0200

    Account page test

--- FRONT/src/pages/Account/component/Account_Informations/Account_Informations.jsx
+++ FRONT/src/pages/Account/component/Account_Informations/Account_Informations.jsx
@@ -2,5 +2,11 @@ import React from "react";
 import style from "./Account_Informations.module.scss";
 
 export default function Account_Informations() {
-  return <div>Account_Informations</div>;
+  return (
+    <div
+      className={`d-flex w-100 flex-fill flex-column align-items-center p-8`}
+    >
+      <h2 className="mb-20">Informations Personnelles</h2>
+    </div>
+  );
 }
--- FRONT/src/pages/Account/component/Historic/Historic.jsx
+++ FRONT/src/pages/Account/component/Historic/Historic.jsx
@@ -2,5 +2,11 @@ import React from "react";
 import style from "./Historic.module.scss";
 
 export default function Historic() {
-  return <div>History</div>;
+  return (
+    <div
+      className={`d-flex w-100 flex-fill flex-column align-items-center p-8`}
+    >
+      <h2 className="mb-20">Historique</h2>
+    </div>
+  );
 }
